8a1ab702fd8e9bd4b593ed9151fd30fbc53bd3bf
Peter Holloway <holloway.p.r@gmail.com>
2022-12-03 09:40:13 +0000

    [python] Start in insert mode for ptpython's vi_mode

--- dot_config/ptpython/config.py
+++ dot_config/ptpython/config.py
@@ -129,7 +129,7 @@ def configure(repl):
     repl.enable_syntax_highlighting = True
 
     # Get into Vi navigation mode at startup
-    repl.vi_start_in_navigation_mode = True
+    repl.vi_start_in_navigation_mode = False
 
     # Preserve last used Vi input mode between main loop iterations
     repl.vi_keep_last_used_mode = False
