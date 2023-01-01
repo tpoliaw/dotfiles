e8f2ba1261427c2b5670535040040dede4926790
Peter Holloway <holloway.p.r@gmail.com>
2023-01-01 20:19:40 +0000

    [zsh] Merge in zsh config from drumknott
    - Add missing plugins (but still use zr instead of antibody)
    - Use ascii for bindkey escape characters so git doesn't think it's a
      binary file.
    - Move environment variables from zshrc to zshenv

--- dot_config/zr.zsh
+++
@@ -1,9 +0,0 @@
-export _ZR=$0
-# junegunn/fzf.git/shell/key-bindings.zsh
-source /home/peter/.cache/zr/junegunn/fzf.git/shell/key-bindings.zsh
-
-# zsh-users/zsh-syntax-highlighting
-source /home/peter/.cache/zr/zsh-users/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-
-autoload -Uz compinit; compinit -iCd $HOME/.zcompdump
-
